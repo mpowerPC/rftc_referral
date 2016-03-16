8d6d24b5201899162de47ae9d885d9bebbb94e5c
web-dev <web-dev@mpower-web.home>
2016-03-16 19:52:57 -0400

    Cleaned up some comments and old code.

--- urls.py
+++ urls.py
@@ -6,10 +6,6 @@ router = DefaultRouter()
 
 router.register(r'add_referral', rftc_referral_views.AddReferralViewSet)
 
-# views no longer in use
-# router.register(r'ttrwindow', data_collector_views.TtrWindowViewSet)
-# router.register(r'ttridle', data_collector_views.TtrIdleViewSet)
-# router.register(r'album', data_collector_views.AlbumViewSet)
 urlpatterns = [
     url(r'^', include(router.urls), name="rftc_referral"),
 ]
--- views.py
+++ views.py
@@ -1,10 +1,10 @@
 from rftc_referral.models import RFTCReferral
 from rftc_referral.serializers import RFTCReferralSerializer
-from rest_framework import viewsets, filters
-
+from rest_framework import viewsets
 
 class AddReferralViewSet(viewsets.ModelViewSet):
     filter_fields = ('user', 'code',)
 
     serializer_class = RFTCReferralSerializer
     queryset = RFTCReferral.objects.all()
+
