8e4706754166f0bf499cb4ea87c57ac1ba26f373
web-dev <web-dev@mpower-web.home>
2016-03-16 20:40:05 -0400

    Random Referral Page Created.

--- views.py
+++ views.py
@@ -1,6 +1,8 @@
 from rftc_referral.models import RFTCReferral
 from rftc_referral.serializers import RFTCReferralSerializer
 from rest_framework import viewsets
+from django.http import HttpResponseRedirect
+import random
 
 class AddReferralViewSet(viewsets.ModelViewSet):
     filter_fields = ('user', 'code',)
@@ -8,3 +10,11 @@ class AddReferralViewSet(viewsets.ModelViewSet):
     serializer_class = RFTCReferralSerializer
     queryset = RFTCReferral.objects.all()
 
+
+def RandomReferral(request):
+    referral_url = "https://robertsspaceindustries.com/enlist?referral="
+    random.seed()
+    winner_index = random.randint(1, RFTCReferral.objects.count())
+    winner = RFTCReferral.objects.get(id=winner_index).code
+
+    return HttpResponseRedirect(referral_url + winner)
